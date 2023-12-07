f0bf4af50918b3521fbd6359c0fe5317c1141670
H153165g <chrn22018@g.nihon-u.ac.jp>
2023-12-08 01:33:48 +0900

    gamebase

--- src/App.jsx
+++ src/App.jsx
@@ -19,99 +19,106 @@ async function fetchCategory(category,searchTerm){
 
 export default function App(){
     const [product,setProduct] = useState([]);
-    const [boxes,setBoxes]=useState([{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}])
-    const [imgdiv,setImgdiv]=useState([{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}])
-    const syokidiv={
-        width: '100%',
-        display: 'block',
-        position: 'absolute',
-        bottom: '0px',
-    }
+    const [start,setStart]=useState(true);
+    const [Qz,setQz]=useState({});
+    const [Sty,setSty]=useState(Array(8).fill([0, 0]))
+
+    const gameseries=[
+        "The Legend of Zelda",
+        "Splatoon",
+        "Mario Sports Superstars",
+        "Monster Hunter",
+        "Kirby",
+        "Animal Crossing",
+        "Pokemon",
+        "Final Fantasy"
+    ];
+
     
     useEffect(() => {
         (async () => {
             const newProduct = await fetchCategory("All","");
+
             setProduct(newProduct);
+            console.log("非同期な処理終了！");
     
           })();
       }, []);
-
-      
-
     
+    function getRandomPosition() {
+        const top = Math.floor(Math.random() * window.innerHeight);
+        const left = Math.floor(Math.random() * window.innerWidth);
+
+        return [
+             top,
+             left 
+        ];
+    }
+    function stylesetting(){
+        const data=[getRandomPosition(),getRandomPosition(),getRandomPosition(),getRandomPosition(),getRandomPosition(),getRandomPosition(),getRandomPosition(),getRandomPosition()];
+        setSty(data);
+    }
       
-      function resetdiv(i) {
-        if (i === null) {
-          setImgdiv(
-            Array(20).fill(syokidiv) // もしくは適切な初期値を持つ配列に置き換える
-          );
-        } else {
-          setImgdiv(
-            imgdiv.map((item, index) => (index === i ? syokidiv : item)) // imgdiv内の特定の要素を置き換える
-          );
-        }
-      }
-
-
-
-      async function randomValueFromArray() {
+    async function randomValueFromArray() {
+        
         if (!product || product.length === 0) {
           // productが空の場合、エラーを避けるために何らかの処理を行う（例: エラーメッセージの設定）
           return { url: '', name: '' }; // 空のオブジェクトやデフォルトの値を返す
         } else {
-          const result =  Math.floor(Math.random() * product.length);
-          const box = {
-            url: product[result].image,
-            name: product[result].character
-          };
-          return box;
+            const result =  Math.floor(Math.random() * product.length);
+            setQz(
+                {
+                    url: product[result].image,
+                    name: product[result].character,
+                    game:product[result].gameSeries
+                }
+            )
+            
         }
-      }
+    }
       
-
-
-      async function box(i) {
-        if (!product || product.length === 0) {
-            console.log("Fuck you")
-          // productが空の場合、エラーを避けるために何らかの処理を行う（例: エラーメッセージの設定）
-          return; // 何もしないか、エラーメッセージを設定するなど
-        }
-        
-        if (i === null) {
-          setBoxes(
-            boxes.map(() => randomValueFromArray())
-          );
-        } else {
-          setBoxes(
-            boxes.map((box, index) => (index === i ? randomValueFromArray() : box))
-          );
-        }
-      }
-      
-    useEffect(()=>{
-        (async () => {
-            box(null);
-            resetdiv(null);
-          })();
-      }, [product]);
-
-
     return(
         <>
         
-        <section>
-        {boxes.map((box,index)=>(
-            
-            
-            <div key={index} className="vegetables">
-            <img src={box.url} alt={box.name} />
-            </div>
-        ))}
+    {start &&
+    <button onClick={
+            async()=>{
+                await randomValueFromArray();
+                stylesetting();
+                console.log(Qz);
+                setStart(false);
+        }} className="first">Start!!</button>
+    }
+    <button onClick={
+        async()=>{
+            await randomValueFromArray();
+            stylesetting();
+            console.log(Sty);
+        }
+    } className="pass">Pass</button>
+
+       <div className="relative">
+          <img src={Qz.url} alt={Qz.name} className="Qz"/>
+        </div>
         
-        </section>
-        <header>
-        <h1>死ね</h1>
-        </header>
+        {gameseries.map((game,index)=>{
+            console.log(game);
+            console.log(Qz.game);
+            return(
+            <button 
+            key={index} 
+            onClick={
+                async()=>{
+                    if(game===Qz.game){
+                        await randomValueFromArray();
+                        stylesetting();
+                    }
+
+                }
+            }  style={{position:'absolute',top:Sty[index][0],left:Sty[index][1]}}>{game}</button>
+            )
+        })}
         </>
     );
+    
 }
